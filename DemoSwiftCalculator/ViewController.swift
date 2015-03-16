039a06290fccf101001f67dd7ffda72ad1c7818d
Julien DAUPHANT <julien@Juliens-Mac-mini.local>
2015-03-16 11:42:25 +0100

    Initial Commit

---
+++ DemoSwiftCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoSwiftCalculator
+//
+//  Created by Julien DAUPHANT on 16/03/15.
+//  Copyright (c) 2015 Siz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
